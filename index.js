8dddbfae55aaed742888479ea9b005aa794af90e
sumanthpweb11 <sumanthpweb11@gmail.com>
2023-02-14 09:22:46 +0530

    a

--- index.js
+++ index.js
@@ -24,10 +24,7 @@ app.use(
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH"
-  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
